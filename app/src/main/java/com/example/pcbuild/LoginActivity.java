8ea77a529c8d2eef8377271fd9d297db031b211e
Daryll Vinuya <daryllvinuya@gmail.com>
2020-12-26 00:04:43 +0800

    Sign in and Sign up Design

--- app/src/main/java/com/example/pcbuild/LoginActivity.java
+++ app/src/main/java/com/example/pcbuild/LoginActivity.java
@@ -2,12 +2,14 @@ package com.example.pcbuild;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
     }
 }
